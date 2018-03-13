dd5aed55475070b09a8d2b0ff76f31d24423abac
Simon <simon.m.poile@gmail.com>
2018-03-13 19:28:22 +0000

    Created the skeleton of the RESTful service.

---
+++ serv.py
@@ -0,0 +1,41 @@
+from flask import Flask, jsonify
+import urllib3
+
+app = Flask(__name__)
+
+tasks = [
+    {
+        'id': 1,
+        'title': u'Buy groceries',
+        'description': u'Milk, Cheese, Pizza, Fruit, Tylenol',
+        'done': False
+    },
+    {
+        'id': 2,
+        'title': u'Learn Python',
+        'description': u'Need to find a good Python tutorial on the web',
+        'done': False
+    }
+]
+
+@app.route('/')
+def index():
+    return "Hello, World!"
+
+
+@app.route('/app/api/v1.0/tasks', methods=['GET'])
+def get_tasks():
+    return jsonify({'tasks' : tasks})
+
+def response(botdata):
+    http = urllib3.PoolManager()
+    DOMAIN= 'http://127.0.0.1:5000'
+    r = http.request('POST',DOMAIN,botdata)
+
+
+
+
+
+
+if __name__ =='__main__':
+    app.run(debug=True)
